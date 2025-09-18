910e34449d82716e6a6cfb46d8db94248c0ac90c
Riya Rastogi <riya02rastogi@gmail.com>
2025-09-18 06:09:58 +0530

    dispatching the action at body with user details

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,25 +1,50 @@
 import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/store/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
 
-    const appRouter = createBrowserRouter([
-        {
-            path:"/",
-            element:<Login/>
-        },
-        {
-            path:"/browse",
-            element:<Browse/>
-        },
-    ])
+  //using firebase api for dispatching an action
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   return (
-    <div className='text-yellow-600'>
-      <RouterProvider router={appRouter}/>
+    <div className="text-yellow-600">
+      <RouterProvider router={appRouter} />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
